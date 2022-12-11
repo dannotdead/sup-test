c7b66d1bd7e4fb4a4862748ba765c298ad85a96a
dannotdead <46273642+dannotdead@users.noreply.github.com>
2022-12-12 02:16:26 +0700

    feat: start design sidebar component

--- src/components/SideBar/SideBar.tsx
+++ src/components/SideBar/SideBar.tsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import AngleArrow from '../../assets/svg/AngleArrow.svg'
+import './styles.scss'
 
 const SideBar: React.FC = () => {
-  return <aside>sidebar</aside>
+  return (
+    <aside className='sidebar-container'>
+      <div className='sidebar-container__header'>
+        <img src={AngleArrow} alt='' />
+        <span>Мой курс приёма</span>
+      </div>
+
+      <div className='sidebar-container__list'></div>
+    </aside>
+  )
 }
 
 export default SideBar
